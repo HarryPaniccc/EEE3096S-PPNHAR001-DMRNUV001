9f84f6ce6b16c34b10733111dd2b13bbff92ddd5
xNiDx <141439014+xNiDx@users.noreply.github.com>
2023-08-21 07:40:28 +0200

    Prac2real

--- Prac2/main.c
+++ Prac2/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdint.h>
+#include "stm32f0xx.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -31,7 +32,23 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define MEM_1 0b000000000000001
+#define MEM_2 0b000000000000010
+#define MEM_3 0b000000000000011
+#define MEM_4 0b000000000000100
+#define MEM_5 0b000000000000101
+#define MEM_6 0b000000000000110
+uint16_t MEM_0 = 0b000000000000000;
+uint8_t ghost = 0; //counter variable for time change
 
+// Definitions for SPI usage
+#define MEM_SIZE 8192 // bytes
+#define WREN 0b00000110 // enable writing
+#define WRDI 0b00000100 // disable writing
+#define RDSR 0b00000101 // read status register
+#define WRSR 0b00000001 // write status register
+#define READ 0b00000011
+#define WRITE 0b00000010
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,6 +60,8 @@
 TIM_HandleTypeDef htim16;
 
 /* USER CODE BEGIN PV */
+// TODO: Define any input variables
+static uint8_t patterns[] = {0b10101010, 0b01010101, 0b11001100, 0b00110011, 0b11110000, 0b00001111};
 
 /* USER CODE END PV */
 
@@ -51,7 +70,12 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM16_Init(void);
 /* USER CODE BEGIN PFP */
-
+void EXTI0_1_IRQHandler(void);
+void TIM16_IRQHandler(void);
+static void init_spi(void);
+static void write_to_address(uint16_t address, uint8_t data);
+static uint8_t read_from_address(uint16_t address);
+static void delay(uint32_t delay_in_us);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -66,7 +90,6 @@ static void MX_TIM16_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -75,20 +98,32 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
+  init_spi();
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM16_Init();
   /* USER CODE BEGIN 2 */
+
+  // TODO: Start timer TIM16
+  HAL_TIM_Base_Start_IT(&htim16);
+  // TODO: Write all "patterns" to EEPROM using SPI
+
+  write_to_address(MEM_1,patterns[0]);
+  write_to_address(MEM_2,patterns[1]);
+  write_to_address(MEM_3,patterns[2]);
+  write_to_address(MEM_4,patterns[3]);
+  write_to_address(MEM_5,patterns[4]);
+  write_to_address(MEM_6,patterns[5]);
+
+
 
   /* USER CODE END 2 */
 
@@ -99,6 +134,23 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+	// TODO: Check button PA0; if pressed, change timer delay
+	 if(HAL_GPIO_ReadPin(GPIOA, Button0_Pin) == 0)
+			{
+				if(ghost == 0)
+				{
+					MX_TIM16_Init();
+					ghost =1;
+
+				}
+				else if (ghost == 1)
+				{
+					MX_TIM16_Init_0_5();
+					ghost = 0;
+				}
+			}
+
   }
   /* USER CODE END 3 */
 }
@@ -148,7 +200,6 @@ static void MX_TIM16_Init(void)
 {
 
   /* USER CODE BEGIN TIM16_Init 0 */
-
   /* USER CODE END TIM16_Init 0 */
 
   /* USER CODE BEGIN TIM16_Init 1 */
@@ -166,7 +217,7 @@ static void MX_TIM16_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN TIM16_Init 2 */
-
+  NVIC_EnableIRQ(TIM16_IRQn);
   /* USER CODE END TIM16_Init 2 */
 
 }
@@ -297,6 +348,266 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void PAT1(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
+	LL_GPIO_TogglePin(LED5_GPIO_Port, LED5_Pin);
+	LL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+	LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);//ONES
+
+}
+
+void PAT2(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED6_GPIO_Port, LED6_Pin);
+	LL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
+	LL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+	LL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+
+}
+
+void PAT3(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
+	LL_GPIO_TogglePin(LED6_GPIO_Port, LED6_Pin);
+	LL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+	LL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+
+}
+
+void PAT4(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED5_GPIO_Port, LED5_Pin);
+	LL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
+	LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+	LL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+
+}
+
+void PAT5(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
+	LL_GPIO_TogglePin(LED6_GPIO_Port, LED6_Pin);
+	LL_GPIO_TogglePin(LED5_GPIO_Port, LED5_Pin);
+	LL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
+
+}
+
+void PAT6(void) //turning LEDS per pattern
+{
+	Pin_Reset();
+	LL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+	LL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+	LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+	LL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+
+}
+
+void Pin_Reset(void) //resets all the LED output pins to 0.
+{
+
+	LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+
+  /**/
+	LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+
+	/**/
+	LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+}
+
+
+void MX_TIM16_Init_0_5(void)
+{
+
+  /* USER CODE BEGIN TIM16_Init 0 */
+
+  /* USER CODE END TIM16_Init 0 */
+
+  /* USER CODE BEGIN TIM16_Init 1 */
+
+  /* USER CODE END TIM16_Init 1 */
+  htim16.Instance = TIM16;
+  htim16.Init.Prescaler = 0.5*(8000-1);
+  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim16.Init.Period = (1000-1);
+  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim16.Init.RepetitionCounter = 0;
+  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM16_Init 2 */
+  NVIC_EnableIRQ(TIM16_IRQn);
+  /* USER CODE END TIM16_Init 2 */
+
+}
+
+
+// Initialise SPI
+static void init_spi(void) {
+
+  // Clock to PB
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 	// Enable clock for SPI port
+
+  // Set pin modes
+  GPIOB->MODER |= GPIO_MODER_MODER13_1; // Set pin SCK (PB13) to Alternate Function
+  GPIOB->MODER |= GPIO_MODER_MODER14_1; // Set pin MISO (PB14) to Alternate Function
+  GPIOB->MODER |= GPIO_MODER_MODER15_1; // Set pin MOSI (PB15) to Alternate Function
+  GPIOB->MODER |= GPIO_MODER_MODER12_0; // Set pin CS (PB12) to output push-pull
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; 		// Pull CS high
+
+  // Clock enable to SPI
+  RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+  SPI2->CR1 |= SPI_CR1_BIDIOE; 									// Enable output
+  SPI2->CR1 |= (SPI_CR1_BR_0 |  SPI_CR1_BR_1); 					// Set Baud to fpclk / 16
+  SPI2->CR1 |= SPI_CR1_MSTR; 									// Set to master mode
+  SPI2->CR2 |= SPI_CR2_FRXTH; 									// Set RX threshold to be 8 bits
+  SPI2->CR2 |= SPI_CR2_SSOE; 									// Enable slave output to work in master mode
+  SPI2->CR2 |= (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2); 	// Set to 8-bit mode
+  SPI2->CR1 |= SPI_CR1_SPE; 									// Enable the SPI peripheral
+}
+
+// Implements a delay in microseconds
+static void delay(uint32_t delay_in_us) {
+  volatile uint32_t counter = 0;
+  delay_in_us *= 3;
+  for(; counter < delay_in_us; counter++) {
+    __asm("nop");
+    __asm("nop");
+  }
+}
+
+// Write to EEPROM address using SPI
+static void write_to_address(uint16_t address, uint8_t data) {
+
+	uint8_t dummy; // Junk from the DR
+
+	// Set the Write Enable latch
+	GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
+	delay(1);
+	*((uint8_t*)(&SPI2->DR)) = WREN;
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
+	dummy = SPI2->DR;
+	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+	delay(5000);
+
+	// Send write instruction
+	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
+	delay(1);
+	*((uint8_t*)(&SPI2->DR)) = WRITE;
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+
+	// Send 16-bit address
+	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+
+	// Send the data
+	*((uint8_t*)(&SPI2->DR)) = data;
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
+	dummy = SPI2->DR;
+	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+	delay(5000);
+}
+
+// Read from EEPROM address using SPI
+static uint8_t read_from_address(uint16_t address) {
+
+	uint8_t dummy; // Junk from the DR
+
+	// Send the read instruction
+	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
+	delay(1);
+	*((uint8_t*)(&SPI2->DR)) = READ;
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+
+	// Send 16-bit address
+	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
+	while ((SPI2->SR & SPI_SR_RXNE) == 0);		// Hang while RX is empty
+	dummy = SPI2->DR;
+	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+
+	// Clock in the data
+	*((uint8_t*)(&SPI2->DR)) = 0x42; 			    // Clock out some junk data
+	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
+	dummy = SPI2->DR;
+	GPIOB->BSRR |= GPIO_BSRR_BS_12; 			    // Pull CS high
+	delay(5000);
+
+	return dummy;								              // Return read data
+}
+
+// Timer rolled over
+void TIM16_IRQHandler(void)
+{
+	// Acknowledge interrupt
+	HAL_TIM_IRQHandler(&htim16);
+
+	// TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
+
+
+	uint8_t pat = read_from_address(MEM_0+1);//gets pattern from memory
+
+	if (pat == patterns[0])
+	{
+		PAT1();
+	} else if (pat == patterns[1])
+	{
+		PAT2();
+	} else if (pat == patterns[2])
+	{
+		PAT3();
+	} else if (pat == patterns[3])
+	{
+		PAT4();
+	} else if (pat == patterns[4])
+	{
+		PAT5();
+	} else if (pat == patterns[5])
+	{
+		PAT6();
+	} else
+	{
+		LL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin); //incase of SPI failure
+
+	}
+
+
+	MEM_0 = MEM_0+1;
+	if (MEM_0 == 6)
+	{
+		MEM_0 = 0;
+	}// binary counter and looper
+
+}
 
 /* USER CODE END 4 */
 
